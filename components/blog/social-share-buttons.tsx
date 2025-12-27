9dc1e28dcc43c9144edb63faa6b05f8f81615802
Alexis Olivero <olivero_canario@live.com>
2025-12-27 12:23:24 -0600

    feat: enhance social buttons with outline default style and solid hover effect

--- components/blog/social-share-buttons.tsx
+++ components/blog/social-share-buttons.tsx
@@ -24,13 +24,13 @@ export function SocialShareButtons({ url, title, className = "" }: SocialShareBu
         </svg>
       ),
       href: `https://wa.me/?text=${encodedTitle}%20${encodedUrl}`,
-      className: "bg-[#25D366] hover:bg-[#25D366]/90 text-white border-transparent",
+      className: "bg-transparent border-2 border-[#25D366] text-[#25D366] hover:bg-[#25D366] hover:text-white hover:border-[#25D366]",
     },
     {
       name: "Facebook",
       icon: <Facebook className="w-5 h-5" />,
       href: `https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`,
-      className: "bg-[#1877F2] hover:bg-[#1877F2]/90 text-white border-transparent",
+      className: "bg-transparent border-2 border-[#1877F2] text-[#1877F2] hover:bg-[#1877F2] hover:text-white hover:border-[#1877F2]",
     },
     {
       name: "Messenger",
@@ -40,23 +40,23 @@ export function SocialShareButtons({ url, title, className = "" }: SocialShareBu
         </svg>
       ),
       href: `fb-messenger://share/?link=${encodedUrl}`,
-      className: "bg-[#00B2FF] hover:bg-[#00B2FF]/90 text-white border-transparent",
+      className: "bg-transparent border-2 border-[#00B2FF] text-[#00B2FF] hover:bg-[#00B2FF] hover:text-white hover:border-[#00B2FF]",
     },
     {
       name: "LinkedIn",
       icon: <Linkedin className="w-5 h-5" />,
       href: `https://www.linkedin.com/sharing/share-offsite/?url=${encodedUrl}`,
-      className: "bg-[#0A66C2] hover:bg-[#0A66C2]/90 text-white border-transparent",
+      className: "bg-transparent border-2 border-[#0A66C2] text-[#0A66C2] hover:bg-[#0A66C2] hover:text-white hover:border-[#0A66C2]",
     },
     {
       name: "Reddit",
       icon: (
         <svg viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
-          <path d="M12 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0zm5.01 4.744c.688 0 1.25.561 1.25 1.249a1.25 1.25 0 0 1-2.498.056l-2.597-.547-.8 3.747c1.824.07 3.48.632 4.674 1.488.308-.309.73-.491 1.207-.491.968 0 1.754.786 1.754 1.754 0 .716-.435 1.333-1.01 1.614a3.111 3.111 0 0 1 .042.52c0 2.694-3.13 4.87-7.004 4.87-3.874 0-7.004-2.176-7.004-4.87 0-.183.015-.366.043-.534A1.748 1.748 0 0 1 4.028 12c0-.968.786-1.754 1.754-1.754.463 0 .898.196 1.207.49 1.207-.883 2.878-1.43 4.744-1.487l.885-4.182a.342.342 0 0 1 .14-.197.35.35 0 0 1 .238-.042l2.906.617a1.214 1.214 0 0 1 1.108-.701zM9.25 12C8.561 12 8 12.562 8 13.25c0 .687.561 1.248 1.25 1.248.687 0 1.248-.561 1.248-1.249 0-.688-.561-1.249-1.249-1.249zm5.5 0c-.687 0-1.248.561-1.248 1.25 0 .687.561 1.248 1.249 1.248.688 0 1.249-.561 1.249-1.249 0-.687-.562-1.249-1.25-1.249zm-5.466 3.99a.327.327 0 0 0-.231.094.33.33 0 0 0 0 .463c.842.842 2.484.913 2.961.913.477 0 2.105-.056 2.961-.913a.361.361 0 0 0 .029-.463.33.33 0 0 0-.464 0c-.547.533-1.684.73-2.512.73-.828 0-1.979-.196-2.512-.73a.326.326 0 0 0-.232-.095z"/>
+          <path d="M12 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0zm5.01 4.744c.688 0 1.25.561 1.25 1.249a1.25 1.25 0 0 1-2.498.056l-2.597-.547-.8 3.747c1.824.07 3.48.632 4.674 1.488.308-.309.73-.491 1.207-.491.968 0 1.754.786 1.754 1.754 0 .716-.435 1.333-1.01 1.614a3.111 3.111 0 0 1 .042.52c0 2.694-3.13 4.87-7.004 4.87-3.874 0-7.004-2.176-7.004-4.87 0-.183.015-.366.043-.534A1.748 1.748 0 0 1 4.028 12c0-.968.786-1.754 1.754-1.754.463 0 .898.196 1.207.49 1.207-.883 2.878-1.43 4.744-1.487l.885-4.182a.342.342 0 0 1 .14-.197.35.35 0 0 1 .238-.042l2.906.617a1.214 1.214 0 0 1 1.108-.701zM9.25 12C8.561 12 8 12.562 8 13.25c0 .687.561 1.248 1.25 1.248.687 0 1.248-.561 1.248-1.249 0-.688-.561-1.249-1.249-1.249zm5.5 0c-.687 0-1.248.561-1.248 1.25 0 .687.561 1.248 1.249 1.249.688 0 1.249-.561 1.249-1.249 0-.687-.562-1.249-1.25-1.249zm-5.466 3.99a.327.327 0 0 0-.231.094.33.33 0 0 0 0 .463c.842.842 2.484.913 2.961.913.477 0 2.105-.056 2.961-.913a.361.361 0 0 0 .029-.463.33.33 0 0 0-.464 0c-.547.533-1.684.73-2.512.73-.828 0-1.979-.196-2.512-.73a.326.326 0 0 0-.232-.095z"/>
         </svg>
       ),
       href: `https://reddit.com/submit?url=${encodedUrl}&title=${encodedTitle}`,
-      className: "bg-[#FF4500] hover:bg-[#FF4500]/90 text-white border-transparent",
+      className: "bg-transparent border-2 border-[#FF4500] text-[#FF4500] hover:bg-[#FF4500] hover:text-white hover:border-[#FF4500]",
     },
     {
       name: "X (Twitter)",
@@ -66,13 +66,13 @@ export function SocialShareButtons({ url, title, className = "" }: SocialShareBu
         </svg>
       ),
       href: `https://twitter.com/intent/tweet?url=${encodedUrl}&text=${encodedTitle}`,
-      className: "bg-black hover:bg-black/90 text-white border-transparent dark:bg-white dark:text-black dark:hover:bg-white/90",
+      className: "bg-transparent border-2 border-black text-black hover:bg-black hover:text-white hover:border-black dark:border-white dark:text-white dark:hover:bg-white dark:hover:text-black dark:hover:border-white",
     },
     {
       name: "Email",
       icon: <Mail className="w-5 h-5" />,
       href: `mailto:?subject=${encodedTitle}&body=${encodedUrl}`,
-      className: "bg-gray-500 hover:bg-gray-600 text-white border-transparent",
+      className: "bg-transparent border-2 border-gray-500 text-gray-500 hover:bg-gray-500 hover:text-white hover:border-gray-500",
     },
   ]
 
